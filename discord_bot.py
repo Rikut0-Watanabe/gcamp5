0e43d0d58a213773389e205bdcf15f40b213f30c
s1280205 <86813317+Nemuke78@users.noreply.github.com>
2022-05-21 15:41:37 +0900

    Add files via upload

---
+++ discord_bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from getsearchresult import *
+
+TOKEN = "ODkzMTYwMzI0MzgxOTY2Mzg3.GInHJJ.nJXIA_YZAssQV4itGVTd3NPrUA28YbTfWWtzKw"
+bot = commands.Bot(command_prefix="!")
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print("起動完了")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("test.ok!")
+
+async def search(ctx, query):
+    result = get_search_result(query)
+    result_items_list = summarize_search_results(result)
+    for i in range(0, 3):
+        await ctx.send(str(i) + ". " + str(result_items_list[i].title) + "\n" + str(result_items_list[i].url))
+
+
+bot.run(TOKEN)
