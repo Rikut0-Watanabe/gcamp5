6a56fd3a6635329482e817096a3791c55346e9d1
Rikut0-Watanabe <boobee29niku@gmail.com>
2022-05-21 13:33:30 +0900

    inputdataでdiscordからの入力を受け取り、urlを返す(まだ)

--- getsearchresult.py
+++ getsearchresult.py
@@ -5,20 +5,22 @@ CUSTOM_SEARCH_ENGINE_ID = "6e65c67507ca027b1"
 API_KEY = "AIzaSyDKUoyl-6b7hRyk_5wBAJ2RFZw7smqhRf0"
 
 def get_search_results(query):
-    search = build(
+   search = build(
         "customsearch",
         "v1",
         developerkey = API_KEY
-    )
+   )
 
-    result = search.cse().list(
-        q = query,
-        cx = CUSTOM_SEARCH_ENGINE_ID,
-        lr = 'lang_ja'
-        num = 5,
-        start = 1
-    ).execute()
-    return result
+   # Google Custom Search から結果を取得
+   # 詳細: https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
+   result = search.cse().list(
+       q = query,
+       cx = CUSTOM_SEARCH_ENGINE_ID,
+       lr = 'lang_ja',
+       num = 10,
+       start = 1
+   ).execute()
+   return result
 
 def summarize_search_results(result):
 
@@ -28,23 +30,23 @@ def summarize_search_results(result):
    # 抽出した検索結果の情報はこのリストにまとめる
    result_items = []
    
-   # 今回は (start =) 1 個目の結果から (num =) 5 個の結果を取得した
-    for i in range(0, 5):
+   # 今回は (start =) 1 個目の結果から (num =) 3 個の結果を取得した
+   for i in range(0, 3):
         # i番目の検索結果の部分
-        result_item = result_items_part[i]
-        # i番目の検索結果からそれぞれの属性の情報をResultクラスに格納して
-        # result_items リストに追加する
-        result_items.append(
+       result_item = result_items_part[i]
+       # i番目の検索結果からそれぞれの属性の情報をResultクラスに格納して
+       # result_items リストに追加する
+       result_items.append(
             SearchResult(
                 title = result_item['title'],
                 url = result_item['link'],
                 snippet = result_item['snippet'],
                 rank = i + 1
-            )
-        )
+           )
+       )
 
    # 結果を格納したリストを返却
-    return result_items
+   return result_items
 
    # 検索結果の情報を格納するクラス
 class SearchResult:
@@ -55,7 +57,7 @@ class SearchResult:
        self.rank = rank
     def __str__(self):
        # コマンドライン上での表示形式はご自由にどうぞ
-       return "[title] " + self.title + "\n\t[url] " + self.url + "\n\t[snippet] " + self.snippet + "\n\t[rank] " + str(self.rank)
+       return "[title] " + self.title + "\n\t[url] " + self.url
 
 # メインプロセス       
 if __name__ == '__main__':
@@ -70,5 +72,5 @@ if __name__ == '__main__':
    result_items_list = summarize_search_results(result) # result_items_list には SearchResult のリストが入る
 
    # コマンドラインに検索結果の情報を出力
-   for i in range(0, 5):
+   for i in range(0, 3):
        print(result_items_list[i])
