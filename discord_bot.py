1c4aef59bf93da7fe8aff180ca61c77d5b660504
s1280205 <86813317+Nemuke78@users.noreply.github.com>
2022-05-21 21:07:20 +0900

    Add files via upload

--- discord_bot.py
+++ discord_bot.py
@@ -1,3 +1,4 @@
+from http import client
 import discord
 from discord.ext import commands
 from testsearch import *
@@ -16,6 +17,11 @@ async def test(ctx):
     await ctx.send("test.ok!")
 
 @bot.command()
+async def setup(ctx, guild_id):
+    
+    await ctx.send("セットアップ完了です！")
+
+@bot.command()
 async def s(ctx, query):
     await ctx.send("\"" + query + "\"" + "の検索結果を表示します：")
     result = get_search_results(query)
@@ -29,5 +35,25 @@ async def d(ctx, dnum, dface):
     for i in range(0, int(dnum)):
         result_dice = random.randint(1, int(dface))
         await ctx.send(">" + str(result_dice))
+
+    
+@bot.command()
+async def t(ctx, tnum, pnum, guild_id, channel1_id, channel2_id):
+    state = ctx.author.voice
+    if state is None:
+        await ctx.send("チーム振り分け用のボイスチャンネルに入室して下さい")
+
+    else:
+        guild = bot.get_guild(int(guild_id))
+        channel1 = guild.get_channel(int(channel1_id))
+        channel2 = guild.get_channel(int(channel2_id))
+        channel = [channel1, channel2]
+        channel_mem = ctx.author.voice.channel.members
+        random.shuffle(channel_mem)
+        for i in range(0, int(pnum)):
+            chan_num = i % int(tnum)
+            await channel_mem[i].move_to(channel[chan_num])
+        await ctx.send("振り分け完了")
+
 
 bot.run(TOKEN)
