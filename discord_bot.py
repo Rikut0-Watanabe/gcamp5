5ac9216b5f4508b4ba17820fd574a76cf31d2e58
s1280205 <86813317+Nemuke78@users.noreply.github.com>
2022-05-21 18:25:37 +0900

    Add files via upload

--- discord_bot.py
+++ discord_bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 from testsearch import *
+import random
 
-TOKEN = "OTc3NDQ4NjA2NzIwMjA4OTc2.GOCC__.GkJ4o2whOx80uURQJG9j7Hs2i8Z9U9uyWKb6wA"
+TOKEN = "ODkzMTYwMzI0MzgxOTY2Mzg3.GDSXSN.BEVPFsExl_X1Fu-oc5mqP7CvVGoGFNhnOuivA8"
 bot = commands.Bot(command_prefix="!")
 bot.remove_command("help")
 
@@ -16,10 +17,17 @@ async def test(ctx):
 
 @bot.command()
 async def s(ctx, query):
+    await ctx.send("\"" + query + "\"" + "の検索結果を表示します：")
     result = get_search_results(query)
     result_items_list = summarize_search_results(result)
     for i in range(0, 3):
         await ctx.send(str(i+1) + ". " + str(result_items_list[i].title) + "\n" + str(result_items_list[i].url))
 
+@bot.command()
+async def d(ctx, dnum, dface):
+    await ctx.send("ダイス結果を表示します：")
+    for i in range(0, int(dnum)):
+        result_dice = random.randint(1, int(dface))
+        await ctx.send(">" + str(result_dice))
 
 bot.run(TOKEN)
