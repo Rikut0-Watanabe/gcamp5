88d07de440744a3b3dadc3ef041c06ba55cb50ae
Rikut0-Watanabe <boobee29niku@gmail.com>
2022-05-21 17:34:28 +0900

    testsearchの方をincludeしてます

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from getsearchresult import *
+from testsearch import *
 
 TOKEN = "OTc3NDQ4NjA2NzIwMjA4OTc2.GOCC__.GkJ4o2whOx80uURQJG9j7Hs2i8Z9U9uyWKb6wA"
 bot = commands.Bot(command_prefix="!")
@@ -19,7 +19,7 @@ async def s(ctx, query):
     result = get_search_results(query)
     result_items_list = summarize_search_results(result)
     for i in range(0, 3):
-        await ctx.send(str(i) + ". " + str(result_items_list[i].title) + "\n" + str(result_items_list[i].url))
+        await ctx.send(str(i+1) + ". " + str(result_items_list[i].title) + "\n" + str(result_items_list[i].url))
 
 
 bot.run(TOKEN)
