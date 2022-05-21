4a087e0a77a85eda14d31faadce94f5578517905
Rikut0-Watanabe <boobee29niku@gmail.com>
2022-05-21 17:20:02 +0900

    naosimasita

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from getsearchresult import *
 
-TOKEN = ""
+TOKEN = "OTc3NDQ4NjA2NzIwMjA4OTc2.GOCC__.GkJ4o2whOx80uURQJG9j7Hs2i8Z9U9uyWKb6wA"
 bot = commands.Bot(command_prefix="!")
 bot.remove_command("help")
 
@@ -14,8 +14,9 @@ async def on_ready():
 async def test(ctx):
     await ctx.send("test.ok!")
 
-async def search(ctx, query):
-    result = get_search_result(query)
+@bot.command()
+async def s(ctx, query):
+    result = get_search_results(query)
     result_items_list = summarize_search_results(result)
     for i in range(0, 3):
         await ctx.send(str(i) + ". " + str(result_items_list[i].title) + "\n" + str(result_items_list[i].url))
